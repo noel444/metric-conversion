0757dfec166198c7176275d8a3a26c0235901c74
noel444 <38759798+noel444@users.noreply.github.com>
2018-04-26 15:50:57 +0100

    Question 2 Part A
    This is a commit for the entire working converter with comments.

--- G00347796/js/convert.js
+++ G00347796/js/convert.js
@@ -1,10 +1,51 @@
+//Metric conversion javascript code
 function fToM(){
 
   var measure = parseInt(document.getElementById("value1").value);
-
+  
+//variable that does the conversion
   var meters = measure * 0.3048;
+  //variable that displays the message of the calculations
   var message = measure + ' feet converts to ' + meters + ' meters.';
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
+
+}
+
+function IToC(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+  
+//variable that does the conversion
+  var centimeters = measure * 2.54;
+  var message = measure + ' inches converts to ' + centimeters + ' centimeters.';
+  console.log(message);
+  document.getElementById("resultsentence1").innerHTML = message;
+
+}
+
+function YToM(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+  
+//variable that does the conversion
+  var meters = measure * 0.9144;
+  //variable that displays the message of the calculations
+  var message = measure + ' yards converts to ' + meters + ' meters.';
+  console.log(message);
+  document.getElementById("resultsentence2").innerHTML = message;
+
+}
+
+function MilesToK(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+  
+//variable that does the conversion
+  var kilometers = measure * 1.60934;
+  //variable that displays the message of the calculations
+  var message = measure + ' miles converts to ' + kilometers + ' kilometers.';
+  console.log(message);
+  document.getElementById("resultsentence3").innerHTML = message;
 
 }
