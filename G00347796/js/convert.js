00541e2662598a38dee98fdce950f27c480cec2a
noel444 <38759798+noel444@users.noreply.github.com>
2018-04-26 16:13:16 +0100

    Added and updated some comments
    I changed some comments to make them more accurate, and I also add some more comments to explain new parts of my code.

--- G00347796/js/convert.js
+++ G00347796/js/convert.js
@@ -3,17 +3,20 @@ function fToM(){
 
   var measure = parseInt(document.getElementById("value1").value);
   
-//variable that does the conversion
+//This variable that does the conversion
   var meters = measure * 0.3048;
-  //variable that displays the message of the calculations
+  
+  //This variable that displays the message of the calculations
   var message = measure + ' feet converts to ' + meters + ' meters.';
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
   
+  //This alerts the user if they do not enter a value
   if (!measure) {
 	  alert("Please enter a value");
   }
   
+  //This alerts the user if they enter a value less than zero
   else if (measure < 0) {
 	  alert("Please enter a value greater than zero");
   }
@@ -24,16 +27,20 @@ function IToC(){
 
   var measure = parseInt(document.getElementById("value1").value);
   
-//variable that does the conversion
+//This variable that does the conversion
   var centimeters = measure * 2.54;
+  
+  //This variable that displays the message of the calculations
   var message = measure + ' inches converts to ' + centimeters + ' centimeters.';
   console.log(message);
   document.getElementById("resultsentence1").innerHTML = message;
   
+  //This alerts the user if they do not enter a value
    if (!measure) {
 	  alert("Please enter a value");
   }
   
+  //This alerts the user if they enter a value less than zero
   else if (measure < 0) {
 	  alert("Please enter a value greater than zero");
   }
@@ -44,17 +51,20 @@ function YToM(){
 
   var measure = parseInt(document.getElementById("value1").value);
   
-//variable that does the conversion
+//This variable that does the conversion
   var meters = measure * 0.9144;
-  //variable that displays the message of the calculations
+  
+  //This variable that displays the message of the calculations
   var message = measure + ' yards converts to ' + meters + ' meters.';
   console.log(message);
   document.getElementById("resultsentence2").innerHTML = message;
   
+  //This alerts the user if they do not enter a value
    if (!measure) {
 	  alert("Please enter a value");
   }
   
+  //This alerts the user if they enter a value less than zero
   else if (measure < 0) {
 	  alert("Please enter a value greater than zero");
   }
@@ -65,17 +75,19 @@ function MilesToK(){
 
   var measure = parseInt(document.getElementById("value1").value);
   
-//variable that does the conversion
+//This variable that does the conversion
   var kilometers = measure * 1.60934;
-  //variable that displays the message of the calculations
+  //This variable that displays the message of the calculations
   var message = measure + ' miles converts to ' + kilometers + ' kilometers.';
   console.log(message);
   document.getElementById("resultsentence3").innerHTML = message;
   
+  //This alerts the user if they do not enter a value
    if (!measure) {
 	  alert("Please enter a value");
   }
   
+  //This alerts the user if they enter a value less than zero
   else if (measure < 0) {
 	  alert("Please enter a value greater than zero");
   }
